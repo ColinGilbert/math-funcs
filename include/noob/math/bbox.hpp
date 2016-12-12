5695677385c83f1fd36b7000d896c3e55178e2c3
Colin <colingilbert86@gmail.com>
2016-12-11 18:26:28 -0600

    Forgot namespace :)

--- include/noob/math/bbox.hpp
+++ include/noob/math/bbox.hpp
@@ -2,18 +2,21 @@
 
 #include "vec3.hpp"
 
-template <typename T>
-struct bbox_type
+namespace noob
 {
-	noob::vec3_type<T> get_dims() const noexcept(true)
-	{
-		return noob::vec3_type<T>(std::fabs(min.v[0]) + std::fabs(max.v[0]), std::fabs(min.v[1]) + std::fabs(max.v[1]), std::fabs(max.v[2]) + std::fabs(max.v[2]));
-	}
+	template <typename T>
+		struct bbox_type
+		{
+			noob::vec3_type<T> get_dims() const noexcept(true)
+			{
+				return noob::vec3_type<T>(std::fabs(min.v[0]) + std::fabs(max.v[0]), std::fabs(min.v[1]) + std::fabs(max.v[1]), std::fabs(max.v[2]) + std::fabs(max.v[2]));
+			}
 
-	void reset() noexcept(true)
-	{
-		min = max = noob::vec3_type<T>(0.0, 0.0, 0.0);
-	}
+			void reset() noexcept(true)
+			{
+				min = max = noob::vec3_type<T>(0.0, 0.0, 0.0);
+			}
 
-	noob::vec3_type<T> min, max;
-};
+			noob::vec3_type<T> min, max;
+		};
+}
